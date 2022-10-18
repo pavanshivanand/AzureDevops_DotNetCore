5273cd534f18865f0f2a09e4cf4fa01669cc053d
pavan <pavanshivanand@gmail.com>
2022-10-18 18:39:46 +0530

    first commit

---
+++ src/Store.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Store.ApplicationCore.Interfaces;
+using Store.Infrastructure.Persistence.Contexts;
+using Store.Infrastructure.Persistence.Repositories;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Store.Infrastructure
+{
+    [ExcludeFromCodeCoverage]
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<StoreContext>(options =>
+               options.UseSqlServer(defaultConnectionString));
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            try
+            {
+                var dbContext = serviceProvider.GetRequiredService<StoreContext>();
+                dbContext.Database.Migrate();
+            }
+            catch
+            {
+            }
+
+            return services;
+        }
+    }
+}
